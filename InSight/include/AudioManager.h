fd8bfba3433d4553d12a818f69168e83c55bb281
I Hudson <cuffy94@gmail.com>
2019-05-26 12:34:42 +0100

    Add fmod deps

---
+++ InSight/include/AudioManager.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#ifndef __AUDIO_MANAGER_H__
+#define __AUDIO_MANAGER_H__
+
+#include "glm/glm.hpp"
+#include <string>
+#include <map>
+#include <vector>
+#include <math.h>
+#include <iostream>
+
+//struct Implementation {
+//	Implementation();
+//	~Implementation();
+//
+//	void Update();
+//
+//	FMOD::Studio::System* mpStudioSystem;
+//	FMOD::System* mpSystem;
+//
+//	int mnNextChannelId;
+//
+//	typedef std::map<std::string, FMOD::Sound*> SoundMap;
+//	typedef std::map<int, FMOD::Channel*> ChannelMap;
+//	typedef std::map<std::string, FMOD::Studio::EventInstance*> EventMap;
+//	typedef std::map<std::string, FMOD::Studio::Bank*> BankMap;
+//
+//	BankMap mBanks;
+//	EventMap mEvents;
+//	SoundMap mSounds;
+//	ChannelMap mChannels;
+//};
+
+class AudioManager
+{
+public:
+
+private:
+};
+
+#endif // __AUDIO_MANAGER_H__
