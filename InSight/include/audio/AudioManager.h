e2956f0e83a0530f38ca49641339c051b0b62a4e
I Hudson <cuffy94@gmail.com>
2019-05-27 18:24:44 +0100

    Started fmod implementation

---
+++ InSight/include/audio/AudioManager.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#ifndef __AUDIO_MANAGER_H__
+#define __AUDIO_MANAGER_H__
+
+#include "glm/glm.hpp"
+#include <string>
+#include <map>
+#include <vector>
+#include <math.h>
+#include <iostream>
+
+//fmod includes
+#include "fmod_studio.hpp"
+#include "fmod.hpp"
+
+struct Implementation {
+	Implementation();
+	~Implementation();
+
+	void Update();
+
+	FMOD::Studio::System* mpStudioSystem;
+	FMOD::System* mpSystem;
+
+	int mnNextChannelId;
+
+	typedef std::map<std::string, FMOD::Sound*> SoundMap;
+	typedef std::map<int, FMOD::Channel*> ChannelMap;
+	typedef std::map<std::string, FMOD::Studio::EventInstance*> EventMap;
+	typedef std::map<std::string, FMOD::Studio::Bank*> BankMap;
+
+	BankMap mBanks;
+	EventMap mEvents;
+	SoundMap mSounds;
+	ChannelMap mChannels;
+};
+
+class AudioManager
+{
+public:
+	AudioManager();
+	~AudioManager();
+
+	static void		init();
+	static void		update();
+	static void		destroy();
+	static int 		errorCheck(FMOD_RESULT aResult);
+
+	void			loadBank(const std::string& strBankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags);
+	void			loadEvent(const std::string& strEventName);
+	void			loadsound(const std::string& strSoundName, bool b3d = true, bool bLooping = false, bool bStream = false);
+	void			unLoadSound(const std::string& strSoundName);
+	void			set3dListenerAndOrientation(const glm::vec3& vPos = glm::vec3(0, 0, 0 ), float fVolumedB = 0.0f);
+	int				playSounds(const std::string &strSoundName, const glm::vec3& vPos = glm::vec3(0, 0, 0), float fVolumedB = 0.0f);
+	void			playEvent(const std::string& strEventName);
+	void			stopChannel(int nChannelId);
+	void			stopEvent(const std::string& strEventName, bool bImmediate = false);
+	void			geteventParameter(const std::string& strEventName, const std::string& strEventParameter, float* parameter);
+	void			setEventParameter(const std::string& strEventName, const std::string& strParameterName, float fValue);
+	void			stopAllChannels();
+	void			setChannel3dPosition(int nChannelId, const glm::vec3& vPosition);
+	void			setChannelvolume(int nChannelId, float fVolumedB);
+	bool			isPlaying(int nChannelId) const;
+	bool			isEventPlaying(const std::string& strEventName) const;
+	float			dbToVolume(float db);
+	float			volumeTodb(float volume);
+	FMOD_VECTOR		vectorToFmod(const glm::vec3& vPosition);
+
+private:
+};
+
+#endif // __AUDIO_MANAGER_H__
---
+++ InSight/src/audio/AudioManager.cpp
@@ -0,0 +1,143 @@
+#include "audio/AudioManager.h"
+
+AudioManager::AudioManager()
+{
+}
+
+AudioManager::~AudioManager()
+{
+}
+
+void AudioManager::init()
+{
+}
+
+void AudioManager::update()
+{
+}
+
+void AudioManager::destroy()
+{
+}
+
+void AudioManager::errorCheck(FMOD_RESULT aResult)
+{
+}
+
+void AudioManager::loadBank(const std::string & strBankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags)
+{
+}
+
+void AudioManager::loadEvent(const std::string & strEventName)
+{
+}
+
+void AudioManager::loadsound(const std::string & strSoundName, bool b3d, bool bLooping, bool bStream)
+{
+}
+
+void AudioManager::unLoadSound(const std::string & strSoundName)
+{
+}
+
+void AudioManager::set3dListenerAndOrientation(const glm::vec3 & vPos, float fVolumedB)
+{
+}
+
+int AudioManager::playSounds(const std::string & strSoundName, const glm::vec3 & vPos, float fVolumedB)
+{
+	return 0;
+}
+
+void AudioManager::playEvent(const std::string & strEventName)
+{
+}
+
+void AudioManager::stopChannel(int nChannelId)
+{
+}
+
+void AudioManager::stopEvent(const std::string & strEventName, bool bImmediate)
+{
+}
+
+void AudioManager::geteventParameter(const std::string & strEventName, const std::string & strEventParameter, float * parameter)
+{
+}
+
+void AudioManager::setEventParameter(const std::string & strEventName, const std::string & strParameterName, float fValue)
+{
+}
+
+void AudioManager::stopAllChannels()
+{
+}
+
+void AudioManager::setChannel3dPosition(int nChannelId, const glm::vec3 & vPosition)
+{
+}
+
+void AudioManager::setChannelvolume(int nChannelId, float fVolumedB)
+{
+}
+
+bool AudioManager::isPlaying(int nChannelId) const
+{
+	return false;
+}
+
+bool AudioManager::isEventPlaying(const std::string & strEventName) const
+{
+	return false;
+}
+
+float AudioManager::dbToVolume(float db)
+{
+	return 0.0f;
+}
+
+float AudioManager::volumeTodb(float volume)
+{
+	return 0.0f;
+}
+
+FMOD_VECTOR AudioManager::vectorToFmod(const glm::vec3 & vPosition)
+{
+	return FMOD_VECTOR();
+}
+
+Implementation::Implementation()
+{
+	mpStudioSystem = NULL;
+	AudioManager::errorCheck(FMOD::Studio::System::create(&mpStudioSystem));
+	AudioManager::errorCheck(mpStudioSystem->initialize(32, FMOD_STUDIO_INIT_LIVEUPDATE, FMOD_INIT_PROFILE_ENABLE, NULL);
+
+	mpSystem = NULL;
+	AudioManager::errorCheck(mpStudioSystem->getCoreSystem(&mpSystem));
+}
+
+Implementation::~Implementation()
+{
+	AudioManager::errorCheck(mpStudioSystem->unloadAll());
+	AudioManager::errorCheck(mpStudioSystem->release());
+}
+
+void Implementation::Update()
+{
+	std::vector<ChannelMap::iterator> pStoppedChannels;
+	for (auto it = mChannels.begin(), itEnd = mChannels.end(); it != itEnd; ++it )
+	{
+		bool bIsPlaying = false;
+		it->second->isPlaying(&bIsPlaying);
+		if (!bIsPlaying)
+		{
+			pStoppedChannels.push_back(it);
+		}
+	}
+
+	for (auto& it : pStoppedChannels)
+	{
+		mChannels.erase(it);
+	}
+	AudioManager::errorCheck(mpStudioSystem->update());
+}
